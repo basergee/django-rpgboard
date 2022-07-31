30a9661fddd3c03d3bb544d1f5bf65ab990cb50e
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-08-01 00:12:44 +0500

    Добавил шаблоны страниц с возможностью авторизоваться

---
+++ rpgboard/board/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import IndexView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('', IndexView.as_view(), name='signup'),
+    path('', IndexView.as_view(), name='profile'),
+    path('', IndexView.as_view(), name='create_post'),
+]
--- rpgboard/board/views.py
+++ rpgboard/board/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class IndexView(ListView):
+    template_name = 'default.html'
+    model = Post
+    context_object_name = 'posts_list'
+    # Ограничиваем количество новостей на странице
+    paginate_by = 10
