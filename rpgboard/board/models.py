5fc2cbc3fb91e0609461f7f7daf043cfdd59e651
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-07-31 10:43:10 +0500

    Добавил описание модели Post

--- rpgboard/board/models.py
+++ rpgboard/board/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    post_types = (
+        ('TK', 'Танки'),
+        ('HL', 'Хилы'),
+        ('DD', 'ДД'),
+        ('TR', 'Торговцы'),
+        ('GM', 'Гилдмастеры'),
+        ('QG', 'Квестгиверы'),
+        ('SM', 'Кузнецы'),
+        ('TN', 'Кожевники'),
+        ('PT', 'Зельевары'),
+        ('SM', 'Мастера заклинаний'),
+    )
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_type = models.CharField(
+        max_length=2,
+        choices=post_types,
+        default='TK'
+    )
+    title = models.TextField()
+    content = models.TextField()
+    upload = models.FileField(upload_to='uploads/')
+    creation_time = models.DateTimeField(auto_now_add=True)
