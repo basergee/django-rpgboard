3949ec9989b93b5ec1f5beb539aa9c88efc212a2
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-08-04 10:59:48 +0500

    Изменил поле на форме создания объявления на виджет tinymce

--- rpgboard/board/forms.py
+++ rpgboard/board/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from tinymce.widgets import TinyMCE
 
 from .models import UserReply, Post
 
@@ -14,6 +15,8 @@ class ReplyForm(forms.ModelForm):
 
 
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
     class Meta:
         model = Post
         fields = [
